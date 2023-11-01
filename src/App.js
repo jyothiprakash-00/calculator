89722a5dab5e6abaa4c397c7b65371841b6b61aa
jyothi <jyothiprakashthundil@gmail.com>
2023-11-01 19:15:35 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <div className="App" id={theme}>
+      
+    <Calculator/>
+    <div className="switch">
+          <label> {theme === "light" ? " " : " "}</label>
+         <div style={{marginTop:'-570px',marginLeft:'500px'}}> <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} /></div>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
+  };
+
+  const { total, next, operation } = state;
+
+  const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+
+  return (
+    <>
+      <section className="cal-container">
+        <h2 data-testid="subtitle">
+          Let's do some
+          {' '}
+          <i> maths!</i>
+        </h2>
+        <div className="calculator">
+          <div className="container">
+            <div className="display" data-testid="display">
+              {total}
+              {operation}
+              {next}
+            </div>
+            <div className="keys">
+              {buttons.map((button) => (
+                <button type="button" className="btn" onClick={handleClick} key={button} data-testid={button}>{button}</button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  );
+};
+
+export default Calculator;
